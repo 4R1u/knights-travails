4b72721793db8f744c3a376cdcf64cf65f0c8442
4R1u <62248461+4R1u@users.noreply.github.com>
2024-12-13 09:44:29 +0400

    Implement knight_moves_arr

--- main.rb
+++ main.rb
@@ -6,3 +6,15 @@ def connected_squares(coords)
   offsets.map { |offset| [coords[0] + offset[0], coords[1] + offset[1]] }
          .filter { |move| (0..7).cover?(move[0]) && (0..7).cover?(move[1]) }
 end
+
+def knight_moves_arr(start, finish)
+  queue = [[start, []]]
+  loop do
+    return (queue[0][1]) if queue[0][0] == finish
+
+    connected_squares(queue[0][0]).each do |move|
+      queue << [move, queue[0][1] + [queue[0][0]]]
+    end
+    queue.delete_at 0
+  end
+end
