74c0d6836ca9108ee6f1c7c3c5efbfcb17872b25
4R1u <62248461+4R1u@users.noreply.github.com>
2024-12-13 09:35:05 +0400

    Implement connected_squares

--- main.rb
+++ main.rb
@@ -1,1 +1,8 @@
 # frozen_string_literal: true
+
+def connected_squares(coords)
+  offsets = [[1, 2], [2, 1], [2, -1], [1, -2],
+             [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
+  offsets.map { |offset| [coords[0] + offset[0], coords[1] + offset[1]] }
+         .filter { |move| (0..7).cover?(move[0]) && (0..7).cover?(move[1]) }
+end
